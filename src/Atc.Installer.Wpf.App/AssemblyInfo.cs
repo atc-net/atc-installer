7408e108ebda64a89d450029290b57fc8aae3fe3
David Kallesen <david@lmdk.dk>
2023-06-29 15:43:45 +0200

    feat: Set GenerateAssemblyInfo=false and add the manuelt to control version

--- src/Atc.Installer.Wpf.App/AssemblyInfo.cs
+++ src/Atc.Installer.Wpf.App/AssemblyInfo.cs
@@ -1,3 +1,13 @@
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None,
     ResourceDictionaryLocation.SourceAssembly)]
+
+[assembly: AssemblyCompany("atc-net")]
+[assembly: AssemblyProduct("Atc.Installer")]
+[assembly: AssemblyTitle("Atc.Installer")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyInformationalVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: System.Resources.NeutralResourcesLanguage("en")]
+[assembly: System.Runtime.Versioning.TargetPlatform("Windows7.0")]
+[assembly: System.Runtime.Versioning.SupportedOSPlatform("Windows7.0")]
